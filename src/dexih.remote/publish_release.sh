637b8743a3135b482188d42ca8db8757444e6669
garyholland <gholland@dataexpertsgroup.com>
2017-12-18 07:59:01 +1100

    updating publish script

--- src/dexih.remote/publish_release.sh
+++ src/dexih.remote/publish_release.sh
@@ -13,45 +13,45 @@ dotnet publish -c release -r osx-x64 -f netcoreapp2.0 --version-suffix C$VERSION
 #dotnet restore -r linux-x64
 dotnet publish -c release -r linux-x64 -f netcoreapp2.0 --version-suffix C$VERSION
 
-if [ ! -d ../dexih.api/wwwroot/remote.binaries ]; 
-then
- mkdir ../dexih.api/wwwroot/remote.binaries
-fi
+# if [ ! -d ../dexih.api/wwwroot/remote.binaries ]; 
+# then
+#  mkdir ../dexih.api/wwwroot/remote.binaries
+# fi
 
 # if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.windows.zip ]; 
 # then
 #  rm ..\dexih.api\wwwroot\remote.binaries\dexih.remote.windows.zip
 # fi
 
-if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.windows.netcore.zip ]; 
-then
- rm ..\dexih.api\wwwroot\remote.binaries\dexih.remote.windows.netcore.zip
-fi
+# if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.windows.netcore.zip ]; 
+# then
+#  rm ..\dexih.api\wwwroot\remote.binaries\dexih.remote.windows.netcore.zip
+# fi
 
-if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.mac.zip ]; 
-then
- rm ..\dexih.api\wwwroot\remote.binaries\ddexih.remote.mac.zip
-fi
+# if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.mac.zip ]; 
+# then
+#  rm ..\dexih.api\wwwroot\remote.binaries\ddexih.remote.mac.zip
+# fi
 
-if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.linux.zip ]; 
-then
- rm ..\dexih.api\wwwroot\remote.binaries\dexih.remote.linux.zip
-fi
+# if [ -f ..\dexih.api\wwwroot\remote.binaries\dexih.remote.linux.zip ]; 
+# then
+#  rm ..\dexih.api\wwwroot\remote.binaries\dexih.remote.linux.zip
+# fi
 
 # pushd ./bin/release/net46/win7-x64/publish
 # zip -r ../../../../../../dexih.api/Resources/RemoteAgents/dexih.remote.windows.zip *
 # popd
 
 pushd ./bin/release/netcoreapp2.0/win7-x64/publish
-zip -r ../../../../../../dexih.api/wwwroot/remote.binaries/dexih.remote.windows.netcore.zip *
+zip -r ../../../../../releases/dexih.remote.windows.netcore_${VERSION}.zip *
 popd
 
 pushd ./bin/release/netcoreapp2.0/osx-x64/publish
-zip -r ../../../../../../dexih.api/wwwroot/remote.binaries/dexih.remote.mac.zip *
+zip -r ../../../../../releases/dexih.remote.mac_${VERSION}.zip *
 popd
 
 pushd ./bin/release/netcoreapp2.0/linux-x64/publish
-zip -r ../../../../../../dexih.api/wwwroot/remote.binaries/dexih.remote.linux.zip *
+zip -r ../../../../../releases/dexih.remote.linux_${VERSION}.zip *
 popd
 
-cp version.txt ../dexih.api/wwwroot/remote.binaries
+# cp version.txt ../dexih.api/wwwroot/remote.binaries
